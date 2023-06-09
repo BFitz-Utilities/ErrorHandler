39d1293f71489c2d9ad9152c8cf5e6b9da7af1d6
Branden Fitzwater <BFitzh20house@gmail.com>
2023-06-08 21:18:47 -0700

    FEAT: Update StackFrame to nullable

--- ErrorHandler/Error.cs
+++ ErrorHandler/Error.cs
@@ -16,7 +16,7 @@ namespace Fitz.Utilities
 
         public StackTrace StackTrace { get; set; }
 
-        private StackFrame _StackFrame;
+        private StackFrame? _StackFrame;
 
         public Error(string id, string message, StackTrace stackTrace)
         {
@@ -24,12 +24,12 @@ namespace Fitz.Utilities
             Message = message;
             StackTrace = stackTrace;
 
-            _StackFrame = StackTrace.GetFrame(1);
+            _StackFrame = StackTrace.GetFrame(1) ?? StackTrace.GetFrame(0);
         }
 
         public override string ToString()
         {
-            return String.Format("Error: {0} | Message: {1} | Method: {2} | Line: {3} | File: {4}", Id, Message, _StackFrame.GetMethod(), _StackFrame.GetFileLineNumber(), _StackFrame.GetFileName());
+            return String.Format("Error: {0} | Message: {1} | Method: {2} | Line: {3} | File: {4}", Id, Message, _StackFrame?.GetMethod(), _StackFrame?.GetFileLineNumber(), _StackFrame?.GetFileName());
         }
     }
 }
